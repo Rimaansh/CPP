e5b82c153ef8fdc0082370a76fe75b2aedb317e4
Rimaansh Bhargava <rimaansh.bhargava@learner.manipal.edu>
2023-05-08 19:46:45 +0530

    Added Knapsack Using DP

--- knapsackDP.cpp
+++ knapsackDP.cpp
@@ -4,12 +4,12 @@ using namespace std;
 int w[100], v[100];
 int DP[100][100];
 
-int knapsack(int CAP, int n) 
+int knapsack(int capacity, int n) 
 {
    int i, wt;
-   int DP[n + 1][CAP + 1]; //taking +1 cuz first row and column is 0
+   //int DP[n + 1][capacity + 1]; //taking +1 cuz first row and column is 0
    for (i = 0; i <= n; i++) {
-      for (wt = 0; wt <= CAP; wt++) {
+      for (wt = 0; wt <= capacity; wt++) {
          if (i == 0 || wt == 0)
             DP[i][wt] = 0; //setting first row and column to 0
          else if (w[i - 1] <= wt) //weight of current less than max weight of column
@@ -18,7 +18,7 @@ int knapsack(int CAP, int n)
             DP[i][wt] = DP[i - 1][wt];
       }
    }
-   return DP[n][CAP];
+   return DP[n][capacity];
 }
 
 int main()
